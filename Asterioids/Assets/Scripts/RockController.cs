292dd4e273f508e45d96332984b81d647027829d
Wolf Reitsamer <reit@htl-donaustadt.at>
2023-10-21 12:42:32 +0200

    Respawner (Laura), Polygon-Collider (Marcel)

--- Asterioids/Assets/Scripts/RockController.cs
+++ Asterioids/Assets/Scripts/RockController.cs
@@ -1,21 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class RockController : MonoBehaviour
 {
     [SerializeField] private Vector3 direction;
 
     [SerializeField] private float speed;
-    
+
+    private float Left => Camera.main.ViewportToWorldPoint(Vector3.zero).x;
+    private float Right => Camera.main.ViewportToWorldPoint(Vector3.right).x;
+    private float Top => Camera.main.ViewportToWorldPoint(Vector3.up).y;
+    private float Bottom => Camera.main.ViewportToWorldPoint(Vector3.zero).y - lr.bounds.size.y * 0.5f;
+
+    private float SpawnLeft => Left - lr.bounds.size.x * 0.5f;
+    private float SpawnRight => Right + lr.bounds.size.x * 0.5f;
+    private float SpawnTop => Top + lr.bounds.size.y * 0.5f;
+    private float SpawnBottom => Bottom - lr.bounds.size.y * 0.5f;
+
+    private float ScreenHeight => (Top - Bottom);
+    private float ScreenWidth => (Right - Left);
+
+    private LineRenderer lr;
+    private MeshCollider mc;
+
+    private void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        mc = GetComponent<MeshCollider>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         transform.position +=
             new Vector3(
-                direction.x * speed * Time.deltaTime,
-                direction.y * speed * Time.deltaTime,
+                direction.normalized.x * speed * Time.deltaTime,
+                direction.normalized.y * speed * Time.deltaTime,
                 0
             );
+
+        CorrectPosition();
+    }
+
+    private void CorrectPosition()
+    {
+        // if (direction.normalized.x < 0)
+        // {
+        //     if (lr.bounds.max.x < Left)
+        //     {
+        //         transform.position = 
+        //             new Vector3(
+        //                 Right + lr.bounds.size.x * 0.5f,
+        //                 transform.position.y,
+        //                 transform.position.z);
+        //     }
+        // }
+
+        transform.position += direction.normalized * Time.deltaTime * speed;
+        transform.position = new Vector3(
+            (transform.position.x + SpawnRight + ScreenWidth) % ScreenWidth - SpawnRight,
+            (transform.position.y + SpawnTop + ScreenHeight) % ScreenHeight - SpawnTop,
+            transform.position.z
+        );
     }
 }
--- Asterioids/Assets/Scripts/RockCreator.cs
+++ Asterioids/Assets/Scripts/RockCreator.cs
@@ -5,33 +5,45 @@ using Unity.Mathematics;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-[RequireComponent(typeof(LineRenderer))]
+[RequireComponent(typeof(LineRenderer), typeof(PolygonCollider2D))]
 public class RockCreator : MonoBehaviour
 {
     [SerializeField] private int vertices;
     [SerializeField] private Vector3 initialPos;
     [SerializeField] private float speed;
     [SerializeField] private float threshold;
+    [SerializeField] private bool animate;
 
     private float randomStartDeformation;
+    
+    private LineRenderer lr;
+    // private MeshCollider mc;
+    private PolygonCollider2D pc;
     
     // Start is called before the first frame update
     void Start()
     {
+        lr = GetComponent<LineRenderer>();
+        // mc = GetComponent<MeshCollider>();
+        pc = GetComponent<PolygonCollider2D>();
+        
         randomStartDeformation = Random.Range(0f, 10f);
         GenerateGeometry();
+        GenerateCollider();
     }
 
     // Update is called once per frame
     void Update()
     {
-        GenerateGeometry();   
+        if (animate)
+        {
+            GenerateGeometry();
+            GenerateCollider();
+        }
     }
 
     private void GenerateGeometry()
     {
-        LineRenderer lr = GetComponent<LineRenderer>();
-
         lr.positionCount=vertices;
         lr.startWidth=0.25f;
         lr.endWidth=0.25f;
@@ -56,8 +68,33 @@ public class RockCreator : MonoBehaviour
         lr.loop=true;
     }
 
+    // private void GenerateCollider()
+    // {
+    //     Mesh mesh = new Mesh();
+    //     lr.BakeMesh(mesh);
+    //     mc.sharedMesh = mesh;
+    // }
+
+    private void GenerateCollider()
+    {
+        Vector3[] positions = new Vector3[lr.positionCount];
+        lr.GetPositions(positions);
+
+        Vector2[] positions2d = new Vector2[positions.Length];
+        for (int i = 0; i < positions.Length; i++)
+            positions2d[i] = positions[i];
+        
+        pc.SetPath(0, positions2d);
+    }
+    
     private void OnValidate()
     {
+        lr = GetComponent<LineRenderer>();
+        // mc = GetComponent<MeshCollider>();
+        pc = GetComponent<PolygonCollider2D>();
+
+
         GenerateGeometry();
+        GenerateCollider();
     }
 }
