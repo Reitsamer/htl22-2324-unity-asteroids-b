d46093e39bb44078b0345f347b18ba28c6477b02
Velitheslav <200172@studierende.htl-donaustadt.at>
2023-12-16 12:42:08 +0100

    2 controlls
    2 controlls must be made fixed and horizontal/vertical
    
    needs to be made to work with masls code
    
    button for shooting as well
    (volume up/click on top)

--- Asterioids/Assets/Scripts/SpaceshipController.cs
+++ Asterioids/Assets/Scripts/SpaceshipController.cs
@@ -7,7 +7,10 @@ public class SpaceshipController : MonoBehaviour
     public float speed = 1;
 
     [SerializeField] private Transform bullet;
-    
+
+    public VariableJoystick variableJoystick;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,15 +20,25 @@ public class SpaceshipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(variableJoystick.Direction);
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             // LineRenderer lr = GetComponent<LineRenderer>();
             // Transform transform = GetComponent<Transform>();
             transform.Rotate(new Vector3(0, 0, 1), speed * Time.deltaTime);
+           
         }
         
         if (Input.GetKey(KeyCode.RightArrow))
             transform.Rotate(Vector3.forward, -speed * Time.deltaTime);
+
+        transform.Rotate(new Vector3(0, 0, 1), speed * Time.deltaTime* -variableJoystick.Direction.x);
+
+        // if (true)//variableJoystick.y!=0)
+        //{
+        //transform.Rotate(variableJoystick.Direction, speed * Time.deltaTime);
+        //}
+
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
