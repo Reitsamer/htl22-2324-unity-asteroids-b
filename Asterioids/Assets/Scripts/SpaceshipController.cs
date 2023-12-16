f0a51b9cbfdfb7d431497e5025c4de7bff2b4286
Masl <70035395+marceldobehere@users.noreply.github.com>
2023-12-16 10:40:19 +0100

    a

--- Asterioids/Assets/Scripts/SpaceshipController.cs
+++ Asterioids/Assets/Scripts/SpaceshipController.cs
@@ -7,28 +7,44 @@ public class SpaceshipController : Loopable
     [SerializeField]
     public float RotSpeed = 300;
     [SerializeField]
-    public float MoveSpeed = 10;
+    public float MoveSpeed = 3;
+    [SerializeField]
+    public float MoveSlowdown = 2f;
 
     [SerializeField]
     public GameObject bulletPrefab;
 
     public float Accel = 0;
-    private Vector3 AccelVec = Vector3.zero;
+    private float lastAngle = 0;
+    private bool accelPressed = false;
+    [SerializeField]
+    private List<Vector3> Forces = new List<Vector3>();
 
     private LineRenderer lineRenderer;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         Accel = 0;
+        lastAngle = 0;
+        accelPressed = false;
         lineRenderer = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool space = Input.GetKeyDown(KeyCode.Space);
+        bool left = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+        bool right = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+        bool fw = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+
+
+
         // shoot bullet
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (space)
         {
             var bullet = Instantiate(bulletPrefab);
             bullet.transform.position = transform.position;
@@ -37,35 +53,67 @@ public class SpaceshipController : Loopable
             bullet.transform.Translate(Vector3.up * 2.5f, transform);
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        if (left)
         {
             var pos = transform.eulerAngles;
             pos.z += Time.deltaTime * RotSpeed;
             transform.eulerAngles = pos;
         }
 
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        if (right)
         {
             var pos = transform.eulerAngles;
             pos.z -= Time.deltaTime * RotSpeed;
             transform.eulerAngles = pos;
         }
 
-        bool fw = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
-        bool bw = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
 
-        Accel -= Accel * (1.7f * Time.deltaTime);
-        Accel += ((fw ? 1 : 0) - (bw ? 1 : 0)) * 10f * Time.deltaTime;
-        Accel = Mathf.Clamp(Accel, -3, 5);
+        bool bw = false;// Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
 
+
+        if (fw && !accelPressed)
         {
-            float mult = Accel * Time.deltaTime;
-            var pos = transform.position;
-            var angle = (transform.eulerAngles.z * Mathf.PI) / 180;
-            pos.x -= Mathf.Sin(angle) * MoveSpeed * mult;
-            pos.y += Mathf.Cos(angle) * MoveSpeed * mult;
-            transform.position = pos;
+            lastAngle = (transform.eulerAngles.z * Mathf.PI) / 180;
+            accelPressed = true;
         }
+        accelPressed &= fw;
+
+        if (fw)
+        {
+            float angle = (transform.eulerAngles.z * Mathf.PI) / 180;
+            Vector3 force = Vector3.zero;
+            force.x = -Mathf.Sin(angle) * MoveSpeed;
+            force.y = Mathf.Cos(angle) * MoveSpeed;
+            Forces.Add(force);
+        }
+
+        Vector3 forceSum = Vector3.zero;
+        for (int i = 0; i < Forces.Count; ++i)
+        {
+            Vector3 force = Forces[i];
+            forceSum += force;
+            force *= 0.995f;
+            if (force.magnitude < 0.006f)
+                Forces.RemoveAt(i--);
+            else
+                Forces[i] = force;
+        }
+
+
+        transform.position += forceSum * Time.deltaTime / MoveSlowdown;
+
+        //Accel -= Accel * (1.7f * Time.deltaTime);
+        //Accel += ((fw ? 1 : 0) - (bw ? 1 : 0)) * 10f * Time.deltaTime;
+        //Accel = Mathf.Clamp(Accel, -3, 5);
+
+        //{
+        //    float mult = Accel * Time.deltaTime;
+        //    var pos = transform.position;
+        //    var angle = lastAngle;
+        //    pos.x -= Mathf.Sin(angle) * MoveSpeed * mult;
+        //    pos.y += Mathf.Cos(angle) * MoveSpeed * mult;
+        //    transform.position = pos;
+        //}
 
         CorrectPosition(lineRenderer);
     }
