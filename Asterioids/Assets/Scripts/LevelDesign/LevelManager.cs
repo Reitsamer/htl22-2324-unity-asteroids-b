6857d8f468da910cc00d2c27e66ea05550303904
Nicolas-Seiser <145756665+Nicolas-Seiser@users.noreply.github.com>
2023-12-16 13:14:07 +0100

    LevelManager Rework

--- Asterioids/Assets/Scripts/LevelDesign/LevelManager.cs
+++ Asterioids/Assets/Scripts/LevelDesign/LevelManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -8,19 +10,29 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] private UnityEngine.SceneManagement.Scene active;
+    [SerializeField] private Scene active;                             // represents the active Scene 
+    [SerializeField] private Scene load;                               // loadingscreen can be added   
 
-    private void Start()
+    
+    public event EventHandler<Scene> EndLevel;                // add the logic that should be done if a Level is finished
+    private LinkedList<Scene> sceneList;                           // holds all Levelscenes 
+
+    private void Start()                                                    //All Levels are added to the LList 
     {
-        active = SceneManager.GetActiveScene();
+        for (int i = 0; i < 10; i++)
+        {
+            sceneList.AddLast(SceneManager.GetSceneByName($"Level{i}"));
+        }
     }
 
-    private void Load(Scene scene) => SceneManager.LoadScene(scene.name);
-
-    private void Unload(Scene scene) => SceneManager.UnloadSceneAsync(scene);
-
-    private void OnLevelWasLoaded(int level)
+    private void Update()                                                 //Checks if Level is Complete
     {
-        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(1));
+        if (GameObject.FindGameObjectWithTag("Rock") == null)
+            EndLevel?.Invoke(this, active);
+    }
+
+    private void OnLevelEnd(Scene sender, EventArgs e) //Is Called by EndLevel
+    {
+        
     }
 }
